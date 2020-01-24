9ab9af5b7053e16fc4b0c37040e48a174132c8a0
Nikolay-YeboYebo <nikolay.yeboyebo@gmail.com>
2020-01-24 12:24:10 +0100

    Cambiar el login de la aplicación

--- app_portal__views_portal.py
+++ app_portal__views_portal.py
@@ -3,6 +3,9 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login as login_auth
 from django.utils.decorators import method_decorator
+import hashlib
+
+from models.flfactppal.usuarios import usuarios
 
 
 class sanhigia_pedidos(yblogin):
@@ -32,16 +35,63 @@ class sanhigia_pedidos(yblogin):
             error = ''
         return render(request, 'portal/login.html', {'error': error})
 
+    # def sanhigia_pedidos_auth_login(self, request):
+
+    #     _i = self.iface
+
+    #     if request.method == 'POST':
+    #         action = request.POST.get('action', None)
+    #         username = request.POST.get('username', None).lower()
+
+    #         if action == 'login':
+    #             user = authenticate(username=username, password='Sh20Pda17')
+    #             if user is not None:
+    #                 login_auth(request, user)
+    #                 accessControl.accessControl.registraAC()
+    #             else:
+    #                 return _i.login(request, 'Error de autentificación')
+    #             return HttpResponseRedirect("/")
+    #     return _i.login(request)
+
     def sanhigia_pedidos_auth_login(self, request):
 
         _i = self.iface
 
         if request.method == 'POST':
             action = request.POST.get('action', None)
-            username = request.POST.get('username', None).lower()
+            username = request.POST.get('username', None)
+            password = request.POST.get('password', None)
 
             if action == 'login':
-                user = authenticate(username=username, password='Sh20Pda17')
+                if username == "admin":
+                    user = authenticate(username=username, password=password)
+                    if user is not None:
+                        login_auth(request, user)
+                        accessControl.accessControl.registraAC()
+                    else:
+                        return _i.login(request, 'Error de autentificación')
+                    return HttpResponseRedirect("/")
+                try:
+                    sisuser = User.objects.get(username=username)
+                except Exception as e:
+                    # Si no existe usuario en django lo creamos(Si existe el usuario en tabla usuarios)
+                    try:
+                        usuario = usuarios.objects.filter(idusuario__exact=username)
+                        if len(usuario) == 0:
+                            return _i.login(request, 'No existe el usuario')
+                        user = User.objects.create_user(username=username, password="Sh20Pda17")
+                        user.is_staff = False
+                        user.groups.add(Group.objects.get(name='agentes'))
+                        user.save()
+                    except Exception as e:
+                        print(e)
+                usuario = usuarios.objects.get(idusuario=username)
+                md5passwd = hashlib.md5(password.encode('utf-8')).hexdigest()
+                print("falla por aqui??", usuario.password, md5passwd)
+                if usuario.password != md5passwd:
+                    print("si no")
+                    return _i.login(request, 'Error de autentificación')
+                user = authenticate(username=username, password="Sh20Pda17")
                 if user is not None:
                     login_auth(request, user)
                     accessControl.accessControl.registraAC()
